83f2eb56704a9532c0b10706094500e14c1769be
Katherine Gershfeld <ka.gershfeld@gmail.com>
2016-05-04 00:24:57 -0400

    ugh

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-test: robot.mdl lex.py main.py matrix.py mdl.py display.py draw.py gmath.py yacc.py
-	python main.py robot.mdl
+all: myrobot.mdl lex.py main.py matrix.py mdl.py display.py draw.py gmath.py yacc.py
+	python main.py myrobot.mdl
+
+test: myrobot.mdl lex.py main.py matrix.py mdl.py display.py draw.py gmath.py yacc.py
+	python main.py myrobot.mdl
 
 clean:
 	rm *pyc *out parsetab.py
--- script.py
+++ script.py
@@ -40,7 +40,7 @@ def run(filename):
             tmps=[]
 
         elif command[0] == 'sphere':
-            add_sphere( tmps, command[1], command[2], command[3], command[4], command[5], 5 )
+            add_sphere( tmps, command[1], command[2], command[3], command[4], 5 )
             matrix_mult(stack[-1], tmps)
             draw_polygons(tmps, screen, color)
             tmps=[]
@@ -52,28 +52,39 @@ def run(filename):
             tmps=[]
 
         elif command[0] == 'scale':
-            #
+            s = make_scale( command[1], command[2], command[3] )
+            matrix_mult(stack[-1], s)
+            stack[-1]=s
 
-        elif command[0] == 'translate':
-            #
-
+        elif command[0] == 'move':
+            t = make_translate( command[1], command[2], command[3] )
+            matrix_mult(stack[-1], t)
+            stack[-1]=t
+        
         elif command[0] == 'rotate':
-            #
-
+            angle = command[2] * ( math.pi / 180 )
+            if command[1] == 'x':
+                r = make_rotX( angle )
+            elif command[1] == 'y':
+                r = make_rotY( angle )
+            elif command[1] == 'z':
+                r = make_rotZ( angle )
+            matrix_mult(stack[-1], r)
+            stack[-1]=r
+            
         elif command[0] == 'push':
-            #
+            stack.append(new_matrix())
+            for i in range(4):
+                for j in range(4):
+                    stack[-1][i][j] = stack[-2][i][j]
 
         elif command[0] == 'pop':
-            #
+            stack.pop()
 
         elif command[0] == 'save':
-            #
+            save_extension(screen, command[1])
 
         elif command[0] == 'display':
-            #
+            display(screen)
 
-        elif command[0] == 'translate':
-            #
-
-        elif command[0] == 'rotate':
-            #
+        
